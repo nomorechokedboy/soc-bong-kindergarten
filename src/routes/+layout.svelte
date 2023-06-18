fa728b1caff0bf94ffc0e2ac5777bfbbf301ad9a
nomorechokedboy <haiduongit@protonmail.com>
2023-06-18 11:40:02 +0700

    refactor: split header and footer into components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
-	import '../app.css';
+<script lang="ts">
+	import '../app.css'
+	import '@fontsource/montserrat/vietnamese-300.css'
+	import '@fontsource/montserrat/vietnamese-400.css'
+	import '@fontsource/montserrat/vietnamese-500.css'
+	import '@fontsource/montserrat/vietnamese-600.css'
+	import '@fontsource/montserrat/vietnamese-700.css'
+	import '@fontsource/montserrat/vietnamese-800.css'
+	import '@fontsource/montserrat/vietnamese-900.css'
+	import Footer from './layouts/Footer.svelte'
+	import Header from './layouts/Header.svelte'
 </script>
 
-<slot />
+<div class="grid place-items-center">
+	<div class="relative flex h-full min-h-screen w-full max-w-screen-2xl flex-col md:px-10 lg:p-0">
+		<slot name="header">
+			<Header />
+		</slot>
+		<div class="fixed right-0 top-20 z-40 flex max-w-xs flex-col gap-5 lg:mr-5" />
+		<main class="flex-grow">
+			<slot />
+		</main>
+		<slot name="footer">
+			<Footer />
+		</slot>
+	</div>
+</div>
---
+++ src/routes/layouts/Footer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import EmailIcon from '~icons/tabler/mail-filled'
+	import HouseIcon from '~icons/ph/house-fill'
+
+	const locations: { name: string; address: string; phone: string; email: string }[] = []
+</script>
+
+<footer class="flex flex-col">
+	<section class="flex flex-col gap-5">
+		<div class="flex flex-col">
+			<h2 class="text-base font-bold text-black">New york</h2>
+			<div class="text-base font-medium text-neutral-500">
+				<div class="flex items-center gap-2.5">
+					<p>Huntersville, 957 Hill Hills Suite 941 United States</p>
+				</div>
+				<div class="flex items-center gap-2.5">
+					<HouseIcon />
+					<p>Office: +12(3) 456 7890 1234</p>
+				</div>
+				<div class="flex items-center gap-2.5">
+					<EmailIcon />
+					<p>Support: company@name.com</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</footer>
