c4717ca442f372969cd143a4131e79db017d57db
nomorechokedboy <haiduongit@protonmail.com>
2023-06-26 09:30:44 +0700

    feat: added Burger button

---
+++ src/lib/components/Burger.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let opened: boolean
+	export let onClick: any
+</script>
+
+<button class="py-0.5" on:click={onClick}>
+	<div class="flex flex-col gap-1.5">
+		<div
+			class="relative h-0.5 w-6 bg-black transition duration-300 {opened
+				? 'translate-y-2 rotate-45'
+				: ''}"
+		/>
+		<div class="relative h-0.5 w-6 bg-black transition duration-300 {opened ? 'opacity-0' : ''}" />
+		<div
+			class="relative h-0.5 w-6 bg-black transition duration-300 {opened
+				? '-translate-y-2 -rotate-45'
+				: ''}"
+		/>
+	</div>
+</button>
