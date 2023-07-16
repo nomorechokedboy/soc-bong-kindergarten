e86aa2b55aabeecc06832977e479958bb95577dd
Kiến Tín <tinhau1204@gmail.com>
2023-07-16 09:40:27 +0700

    refactor(section): convert FAQ and WhyChooseUs

--- src/lib/components/Reason/Reason.svelte
+++ src/lib/components/Reason/Reason.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-	export let src: string
-	export let alt = ''
-	export let title: string
-	export let content: string
+	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte'
+	import SvelteMarkdown from 'svelte-markdown'
+	// export let src: string
+	// export let alt = ''
+	// export let title: string
+	// export let content: string
+	export let blok: StoryblokComponent
 </script>
 
-<figure>
-	<img class="h-auto max-w-full" {src} {alt} />
+<figure use:storyblokEditable={blok}>
+	<img class="h-auto max-w-full" src={blok.img?.filename} alt={blok.img?.alt} />
 	<figcaption class="mt-2.5 flex flex-col gap-4">
 		<h4 class="text-base font-semibold uppercase text-indigo-950">
-			{title}
+			{blok.title}
 		</h4>
 		<p class="text-sm font-medium text-neutral-700">
-			{content}
+			<SvelteMarkdown source={blok?.content} />
 		</p>
 	</figcaption>
 </figure>
